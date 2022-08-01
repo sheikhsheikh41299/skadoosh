dd9d5d0346256aee9346738e1ba0a53bf0a7cb37
sheikhsheikh41299 <99462650+sheikhsheikh41299@users.noreply.github.com>
2022-08-01 13:44:38 +0530

    Update compress.bash

--- compress.bash
+++ compress.bash
@@ -12,11 +12,11 @@ name=DND
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
-manifest=https://github.com/DarkNess-reDefined/manifest
+manifest= https://github.com/CyanogenMod/android.git
 
 # Manifest branch.
 # Example: cm-14.0
-branch=n7x
+branch= cm-12.1
 
 ### Finally, execute the stuff. ###
 /bin/bash skadoo.sh $name $manifest $branch
