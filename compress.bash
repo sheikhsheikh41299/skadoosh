4487d979db8e7bc557ba4d81116e16498950c44f
sheikhsheikh41299 <99462650+sheikhsheikh41299@users.noreply.github.com>
2022-08-01 14:09:49 +0530

    Update compress.bash

--- compress.bash
+++ compress.bash
@@ -8,15 +8,15 @@
 ### Manifest Configuration ###
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
-name= CyanogenMod
+name=CyanogenMod
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
-manifest= https://github.com/CyanogenMod/android.git
+manifest=https://github.com/CyanogenMod/android.git
 
 # Manifest branch.
 # Example: cm-14.0
-branch= cm-12.1
+branch=cm-12.1
 
 ### Finally, execute the stuff. ###
 /bin/bash skadoo.sh $name $manifest $branch
